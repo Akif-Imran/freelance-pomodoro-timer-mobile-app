0e44491f1c51d72994d0bbc962da8b25b3dc4f45
Akif-Imran <raiden3322@gmail.com>
2024-02-09 14:33:34 +0500

    app.config.js sound name file path change

--- app.config.js
+++ app.config.js
@@ -43,7 +43,7 @@ export default {
         {
           icon: "./assets/favicon.png",
           color: "#ffffff",
-          sounds: ["./src/assets/sounds/time-up.wav"],
+          sounds: ["./src/assets/sounds/timeUp.wav"],
         },
       ],
       [
