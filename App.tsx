a6db883431f1b5a42652b486295744f3be8cb265
Akif-Imran <raiden3322@gmail.com>
2024-02-10 16:43:04 +0500

    webClientId

--- App.tsx
+++ App.tsx
@@ -37,7 +37,7 @@ SystemUI.setBackgroundColorAsync(colors.white);
 
 GoogleSignin.configure({
   // iosClientId: "175623181296-eflgs5u4g6np51ouvdfqirn7hgp7c3id.apps.googleusercontent.com",
-  webClientId: "175623181296-3kqnotihtca3d6pms4f33oenfbkbttot.apps.googleusercontent.com",
+  webClientId: "175623181296-84d3ujrcoqai0o02te7o2g2n4b8nk4tg.apps.googleusercontent.com",
 });
 
 SplashScreen.preventAutoHideAsync();
